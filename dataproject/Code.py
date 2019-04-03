9ec231a47509080ff4c528782635abbf260a16f9
gnh408 <47359320+gnh408@users.noreply.github.com>
2019-04-03 17:43:10 +0200

    Update Code.py

--- dataproject/Code.py
+++ dataproject/Code.py
@@ -4,21 +4,24 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import ipywidgets as widgets
+
 Dst.get_data(table_id = 'NKN1')
 Dst.get_variables(table_id = 'NKN1')
 df = Dst.get_data(table_id = 'NKN1', variables = {'TRANSAKT':['B1GQK'],'PRISENHED':['LKV_M'],'SÆSON':['Y'],'TID':['*']})
 df
+
 df1 = df.drop('TRANSAKT',1)
 df2 = df1.drop('PRISENHED',1)
 df3 = df2.drop('SÆSON',1)
 df3
+
 time = df3['TID']
 gdp  = df3['INDHOLD']
-
 
 gdp.replace('..',np.nan, inplace=True)
 gdp.dropna(inplace=True)
 gdp1 = gdp.iloc[:].astype(float)
+
 def RollingMean(X):
     plt.figure(figsize=(14,7))
     plt.locator_params(which='x', tight=True, nbins=10)
@@ -30,6 +33,6 @@ def RollingMean(X):
     plt.legend(loc='upper left')
     plt.xticks(time[4:], rotation='vertical')
     plt.locator_params(axis='x', nbins=len(time[4:])/2)
-
 widgets.interact(RollingMean, X=widgets.IntSlider(min=0,max=12,step=1,value=6))
+
 gdp1.rolling(X, center=True).mean()
